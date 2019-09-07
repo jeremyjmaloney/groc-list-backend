e47e6df5f3dfd290ace198970fea796cbd2a745a
Jeremy Maloney <jeremymaloney@Jeremys-MacBook-Pro.local>
2019-09-07 01:22:18 -0400

    Added update route for item

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const PORT = 4000;
 
+const Item = require('./models/items.js');
+
 app.use(cors());
 app.use(bodyParser.json());
 
@@ -31,6 +33,21 @@ itemRoutes.route.get('/', (req, res) => {
 itemRoutes.route.get('/:id', (req, res) => {
   Item.findById(req.params.id, (error, foundItem) => {
     res.json(foundItem);
+  });
+});
+
+itemRoutes.route.post('/add', (req, res) => {
+  Item.create(req.body, (error, createdItem) => {
+    res.status(200).json({
+      status: 200,
+      message: 'created item'
+    });
+  });
+});
+
+itemRoutes.route.post('/update/:id', (req, res) => {
+  Item.findByIdAndUpdate(req.params.id, req.body, {new: true}, (error, updatedItem) => {
+    res.json(updatedItem);
   });
 });
 
